65c8e226b2965f430c19c148c38da3e0fd3662b2
MithransRepos <mithran.itsme@gmail.com>
2019-05-13 15:59:29 +0530

    first commit

---
+++ ArtsyChallenge/ViewControllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  ArtsyChallenge
+//
+//  Created by Mithran Natarajan on 5/13/19.
+//  Copyright © 2019 Mithran Natarajan. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+        
+
+}
---
+++ ArtsyChallenge/ViewControllers/RootViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RootViewController.swift
+//  ArtsyChallenge
+//
+//  Created by Mithran Natarajan on 5/13/19.
+//  Copyright © 2019 Mithran Natarajan. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupMenu()
+    }
+    
+    private func setupMenu() {
+        let homeViewController = HomeViewController()
+        let locationViewController = LocationViewController()
+        homeViewController.tabBarItem = UITabBarItem(title: "Home", image: nil, tag: 0)
+        locationViewController.tabBarItem = UITabBarItem(title: "Map", image: nil, tag: 0)
+        let tabBarList = [homeViewController, locationViewController]
+        viewControllers = tabBarList
+    }
+   
+
+}
