34c80f914da7184eb9499146e50e1b8542aa7001
MithransRepos <mithran.itsme@gmail.com>
2019-05-16 16:16:20 +0530

    grouped

---
+++ ArtsyChallenge/Cells/AuctionCell.swift
@@ -0,0 +1,13 @@
+//
+//  AuctionCell.swift
+//  ArtsyChallenge
+//
+//  Created by Mithran Natarajan on 5/16/19.
+//  Copyright © 2019 Mithran Natarajan. All rights reserved.
+//
+
+import UIKit
+
+class AuctionCell: UICollectionViewCell {
+    
+}
