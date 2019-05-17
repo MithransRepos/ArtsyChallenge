b8973cd2b69c6fe64bbfb030b9aff7e2d8d22cfa
MithransRepos <mithran.itsme@gmail.com>
2019-05-17 16:19:38 +0530

    clusting added

---
+++ ArtsyChallenge/CustomViews/AnnotationView.swift
@@ -0,0 +1,9 @@
+//
+//  AnnotationView.swift
+//  ArtsyChallenge
+//
+//  Created by Mithran Natarajan on 5/17/19.
+//  Copyright © 2019 Mithran Natarajan. All rights reserved.
+//
+
+import Foundation
---
+++ ArtsyChallenge/Extensions/MKMapViewExtentension.swift
@@ -0,0 +1,9 @@
+//
+//  UIMapExtentension.swift
+//  ArtsyChallenge
+//
+//  Created by Mithran Natarajan on 5/17/19.
+//  Copyright © 2019 Mithran Natarajan. All rights reserved.
+//
+
+import Foundation
